65c04745e8cf1932c0e5bc39037d6cb8586b2375
Janne <janne.a.varjo@gmail.com>
2021-10-16 22:46:49 +0300

    shared option for creating per-archive sub-directory

--- projects/Gibbed.Dunia.Packing/UnpackOptions.cs
+++ projects/Gibbed.Dunia.Packing/UnpackOptions.cs
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021 Rick (rick 'at' gibbed 'dot' us)
+﻿/* Copyright (c) 2021 Rick (rick 'at' gibbed 'dot' us)
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
@@ -34,5 +34,6 @@ namespace Gibbed.Dunia.Packing
         public string DifferencePath;
         public bool InvertFilter;
         public bool Verbose;
+        public bool CreateSubDirectory;
     }
 }
